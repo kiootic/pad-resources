d660cd3cf353047aa59fc98751a98e2318c54efb
kiootic <kiootic@gmail.com>
2020-11-26 23:26:44 +0800

    Remove renderer

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,6 +4,4 @@ export const actionFactories: Record<string, () => Promise<Action>> = {
   update: () => import('./update').then((m) => m.main),
   extract: () => import('./extract').then((m) => m.main),
   inspect: () => import('./inspect').then((m) => m.main),
-  render: () => import('./render').then((m) => m.main),
-  play: () => import('./play').then((m) => m.main),
 };
--- src/wpe-webgl.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'wpe-webgl' {
-  export interface WebGLOptions {
-    width: number;
-    height: number;
-  }
-
-  export function init(options: WebGLOptions): WebGLRenderingContext;
-  export function nextFrame(swap: boolean): void;
-}
