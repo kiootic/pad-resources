28e7c3148aad830325f912d192de694b8fa1f4ef
kiootic <kiootic@gmail.com>
2020-11-26 23:24:43 +0800

    Accept output directory in data extraction

--- src/actions/extract.ts
+++ src/actions/extract.ts
@@ -1,44 +1,65 @@
 import { readFileSync, writeFileSync } from 'fs';
+import { glob } from 'glob';
+import minimist from 'minimist';
+import { join } from 'path';
 import { BBIN } from '../models/bbin';
 import { ISA } from '../models/isa';
 import { ISC } from '../models/isc';
 import { TEX } from '../models/tex';
 
-async function extract(buf: Buffer) {
+function writeFile(out: string | undefined, name: string, data: Buffer) {
+  let path = name;
+  if (out) {
+    path = join(out, path);
+  }
+  writeFileSync(path, data);
+}
+
+async function extract(buf: Buffer, out: string | undefined) {
   if (TEX.match(buf)) {
     const tex = TEX.load(buf);
     for (const entry of tex.entries) {
       console.log(entry.name);
       const image = await TEX.decode(entry);
-      writeFileSync(entry.name, image);
+      writeFile(out, entry.name, image);
     }
   } else if (BBIN.match(buf)) {
     const bbin = BBIN.load(buf);
     for (const file of bbin.files) {
-      await extract(file);
+      await extract(file, out);
     }
   } else if (ISC.match(buf)) {
     const isc = ISC.load(buf);
     const fileName = isc.name.substring(isc.name.lastIndexOf('/') + 1);
     console.log(fileName);
-    writeFileSync(fileName, buf);
+    writeFile(out, fileName, buf);
   } else if (ISA.match(buf)) {
     const isa = ISA.load(buf);
     const fileName = isa.name.substring(isa.name.lastIndexOf('/') + 1);
     console.log(fileName);
-    writeFileSync(fileName, buf);
+    writeFile(out, fileName, buf);
   }
 }
 
+interface Args {
+  out: string;
+  _: string[];
+}
+
 export async function main(args: string[]) {
-  if (args.length === 0) {
-    console.log('usage: pad-resources extract <bin files>...');
+  const parsedArgs = minimist(args) as Args;
+  if (parsedArgs._.length === 0) {
+    console.log('usage: pad-resources extract [--out <output directory>] <bin files>...');
     return false;
   }
 
-  for (const file of args) {
+  const files: string[] = [];
+  for (const pattern of parsedArgs._) {
+    files.push(...glob.sync(pattern));
+  }
+  for (const file of files) {
     const buf = readFileSync(file);
-    await extract(buf);
+    await extract(buf, parsedArgs.out);
   }
   return true;
 }
