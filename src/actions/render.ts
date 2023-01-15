0ba33838be1f25910550a6f3783073f848bb9c96
AA <chasehult@gmail.com>
2023-01-15 01:26:12 -0700

    Fix typo (#17)

--- src/actions/render.ts
+++ src/actions/render.ts
@@ -130,7 +130,7 @@ async function render(jsonPath: string, outDir: string, renderSingle: boolean, f
 
 
   let animName = path.basename(jsonPath, path.extname(jsonPath));
-  if (forTsubaki) {animName = base.replace(/^mons_0+/, "");}
+  if (forTsubaki) {animName = animName.replace(/^mons_0+/, "");}
 
   if (renderSingle) {
     await renderImg(path.join(outDir, `${animName}.png`));
